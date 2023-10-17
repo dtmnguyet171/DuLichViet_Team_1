8ad02d40273384bc7b72fb7fb7b543f8783503c4
danhcat <121871520+danhcat@users.noreply.github.com>
2023-10-17 22:31:56 +0700

    crud account

--- src/main/java/com/vti/dulichviet_team_1/reponsitory/IAccountReponsitory.java
+++ src/main/java/com/vti/dulichviet_team_1/reponsitory/IAccountReponsitory.java
@@ -1,4 +1,7 @@
 package com.vti.dulichviet_team_1.reponsitory;
 
-public interface IAccountReponsitory {
+import com.vti.dulichviet_team_1.modal.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAccountReponsitory extends JpaRepository<Account, Integer> {
 }
