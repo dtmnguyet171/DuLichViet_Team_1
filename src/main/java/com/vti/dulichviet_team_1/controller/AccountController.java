161c2879f035045a6d62d0a8a33fa9dcb60ab670
danhcat <121871520+danhcat@users.noreply.github.com>
2023-10-18 18:41:55 +0700

    crud account

--- src/main/java/com/vti/dulichviet_team_1/controller/AccountController.java
+++ src/main/java/com/vti/dulichviet_team_1/controller/AccountController.java
@@ -1,4 +1,44 @@
 package com.vti.dulichviet_team_1.controller;
 
+import com.vti.dulichviet_team_1.modal.entity.Account;
+import com.vti.dulichviet_team_1.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 public class AccountController {
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Account> getAccountById(@PathVariable int id) {
+        return accountService.getAccountById(id);
+    }
+
+    @PostMapping
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+
+    @PutMapping
+    public Account updateAccount(@RequestBody Account account) {
+        return accountService.updateAccount(account);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAccount(@PathVariable int id) {
+        accountService.deleteAccount(id);
+    }
+
 }
--- src/main/java/com/vti/dulichviet_team_1/service/AccountService.java
+++ src/main/java/com/vti/dulichviet_team_1/service/AccountService.java
@@ -1,4 +1,40 @@
 package com.vti.dulichviet_team_1.service;
 
+import com.vti.dulichviet_team_1.modal.entity.Account;
+import com.vti.dulichviet_team_1.reponsitory.IAccountReponsitory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AccountService {
+
+    private final IAccountReponsitory accountRepository;
+
+    @Autowired
+    public AccountService(IAccountReponsitory accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Optional<Account> getAccountById(int id) {
+        return accountRepository.findById(id);
+    }
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Account updateAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public void deleteAccount(int id) {
+        accountRepository.deleteById(id);
+    }
 }
