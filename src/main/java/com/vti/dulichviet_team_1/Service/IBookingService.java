6c5bb44e692d96d62278381264f7e404bab045d5
danhcat <121871520+danhcat@users.noreply.github.com>
2023-10-23 18:17:17 +0700

    update

---
+++ src/main/java/com/vti/dulichviet_team_1/Service/IBookingService.java
@@ -0,0 +1,38 @@
+package com.vti.dulichviet_team_1.service;
+
+
+import com.vti.dulichviet_team_1.modal.entity.Account;
+import com.vti.dulichviet_team_1.modal.entity.Booking;
+import com.vti.dulichviet_team_1.request.BookingCreateRequest;
+import com.vti.dulichviet_team_1.request.BookingSearchRequest;
+import com.vti.dulichviet_team_1.request.BookingUpdateRequest;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface IBookingService {
+
+
+    List<Booking> getAllBooking();
+
+    Page<Booking> search(BookingSearchRequest searchRequest);
+
+    void createBooking(BookingCreateRequest bookingCreateRequest);
+
+    Booking updateBooking(int bookingId, BookingUpdateRequest bookingUpdateRequest);
+
+    void deleteBookingId(int bookingId);
+    //xem lịch sử của từng Account
+
+    List<Booking> getBookingHistoryByAccount(Account account);
+
+//    Page<Booking> finBookings(BookingSearch bookingSearch);
+
+
+//
+//    Page<Booking> getAllBookings(Pageable pageable);
+//
+//    Page<Booking> getAllBookingsWithStatus(BookingStatus status, Pageable pageable);
+
+
+}
---
+++ src/main/resources/sql/db.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS du_lich_viet;
+CREATE DATABASE du_lich_viet;
+use du_lich_viet;
+
+
+
